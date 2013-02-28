d0dfd288d472c8da1eea930d57b9d5bb8469fcb7
Ruslan Popov <ruslan.popov@gmail.com>
2013-02-28 15:26:38 +0400

    Version bumps.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="django-adzone",
-    version="0.2",
-    url="http://github.com/winterweaver/django-adzone",
+    name='django-colorfield',
+    version='0.3',
     description="A django app to manage adverts according to zones on a website.",
-    author="Andre Engelbrecht",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'setuptools',
+    author='Ruslan Popov',
+    author_email='ruslan.popov@gmail.com',
+    url="http://github.com/RaD/django-adzone",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development'
     ],
 )
